42eb27238dc20fef24a739713172a664e20239e3
Gregg Redman <greggredman01@gmail.com>
2022-09-10 14:10:13 +0800

    - Made tests DRY \ - tests for HTML

--- pages/tests.py
+++ pages/tests.py
@@ -3,14 +3,22 @@ from django.test import SimpleTestCase
 from django.urls import reverse
 
 class HomepageTests(SimpleTestCase):
+
+    def setUp(self):
+        url = reverse('home')
+        self.response = self.client.get(url)
+
     def test_url_exists_at_correct_location(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
 
     def test_homepage_url_name(self):
-        response = self.client.get(reverse('home'))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
 
     def test_homepage_template(self):
-        response = self.client.get('/')
-        self.assertTrue(response, 'home.html')
+        self.assertTrue(self.response, 'home.html')
+    
+    def test_homepage_contains_correct_html(self):
+        self.assertContains(self.response, 'home page')
+
+    def test_homepage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, "cold beans aint hot bro")
